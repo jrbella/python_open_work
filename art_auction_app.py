a93d404154b160cf3b2ecad462910f906242ad99
jeffbella <dev.jeffbella@gmail.com>
2020-06-20 10:17:33 -0500

    framework for client built

--- art_auction_app.py
+++ art_auction_app.py
@@ -1,58 +1,82 @@
 class Art:
-  def __init__(self, artist, title, medium, year):
+  def __init__(self, artist, title, medium, year, owner):
     self.artist = artist
-    self. title = title
+    self.title = title
     self.medium = medium
     self.year = year
+    self.owner = owner
+
 
   def __repr__(self):
-    return f"{self.artist},  {self.title}, {self.year}, {self.medium}"
-
-girl_with_mandolin = Art("Picasso, Pablo", "Girl with a Mandolin (Fanny Tellier)", "1910", "oil on canvas")
+    return '%s. "%s", %s, %s, %s, %s'%(self.artist, self.title, self.medium, self.year, self.owner.name, self.owner.location)
 
 class Marketplace:
-  def __init__(self, listings):
-    self.listings = listings
   
-  def __repr__(self):
-    return f"listings are : {self.listings}"
+  def __init__(self):
+    self.listings = []
+
   def add_listing(self, new_listing):
-    return self.listings.append(new_listing)
-  def remove_listing(self, listing):
-    return self.listings.remove(listing)
+    self.listings.append(new_listing)
+
+  def remove_listing(self, expired_listing):
+    self.listings.remove(expired_listing)
 
   def show_listings(self):
-    for i in self.listings:
-      print(f"{i}")
+    for listing in self.listings:
+      print(listing)
 
 class Client:
+
   def __init__(self, name, location, is_museum):
     self.name = name
-    self.location = location
     self.is_museum = is_museum
-  def __repr__(self):
-    return f"{self.name}, {self.location}, {self.is_museum}"
-
+    if is_museum:
+      self.location = location
+    else:
+      self.location = "Private Collection"
   
-#declared variables
-veneer = Marketplace([])
-edytta = Client(name = "Edytta Halpirt", location = "unknown", is_museum = False )
-moma = Client(name = "The MOMA", location = "New York", is_museum = True)
+  def sell_artwork(self, artwork, price):
+    if artwork.owner == self:
+      new_listing = Listing(artwork, price, self)
+      veneer.add_listing(new_listing)
 
-#Testing
-my_stuff = Marketplace(["one", "two"])
-print(my_stuff)
-#test add listing
-my_stuff.add_listing("Three")
-print(my_stuff)
-#test remove listing
-my_stuff.remove_listing("one")
-print(my_stuff)
-#test show listings
-print(my_stuff.show_listings)
-#test veneer
-print(veneer)
-#test edytta
-print(edytta)
-#test moma
-print(moma)
+  def buy_artwork(self, artwork):
+    if artwork.owner != self:
+      art_listing = None
+      for listing in veneer.listings:
+        if listing.art == artwork:
+          art_listing = listing
+          break
+      if art_listing != None:
+        art_listing.art.owner = self
+        veneer.remove_listing(art_listing)
+
+class Listing:
+  
+  def __init__(self, art, price, seller):
+    self.art = art
+    self.price = price
+    self.seller = seller
+  
+  def __repr__(self):
+    return '%s, %s' %(self.art.title, self.price)
+
+
+
+veneer = Marketplace()
+
+veneer.show_listings()
+
+#Clients
+edytta = Client("Edytta Halpirt", None, False)
+moma = Client("MOMA", "New York", True)
+
+girl_with_mandolin = Art("Monet, Claude.", "Vétheuil in the Fog", 1879, "oil on canvas.", edytta)
+
+
+edytta.sell_artwork(girl_with_mandolin, "$6M (USD)")
+
+veneer.show_listings()
+moma.buy_artwork(girl_with_mandolin)
+print(girl_with_mandolin)
+veneer.show_listings()
