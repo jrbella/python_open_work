1bf00f48b84cb6ff331985a962c5713cd85203e6
jeffbella <dev.jeffbella@gmail.com>
2020-07-02 06:44:30 -0500

    updating files

--- disemvowel.py
+++ disemvowel.py
@@ -1,7 +1,8 @@
 def disemvowel(string):
     new_string = string.replace("e", '')
     print(new_string)
+    return new_string
 
 
-
+#call function
 test_case = disemvowel("This website is for Losers LOL!!")
