86c40d1447be628a15e000769251835b9c631e6d
jeffbella <dev.jeffbella@gmail.com>
2020-07-02 10:41:04 -0500

    adding file directory to store text files

--- file_processing.py
+++ file_processing.py
@@ -2,8 +2,21 @@
 import os
 
 here = os.path.dirname(os.path.abspath(__file__))
+
+""" Old Code
 myfile = open("%s\\fruit.txt" % (here))
 content = myfile.read()
 myfile.close()
+"""
+
+#adding with context manager
+with open("%s\\fruit.txt" % (here)) as myfile:
+    content = myfile.read()
 
 print(content)
+
+#retrieving from file directory
+with open("%s/files/fruit_2.txt" % (here)) as myfile:
+    content_2 = myfile.read()
+
+print(content_2)
