c16c1da6c0e3fb20d256735181969c4a9682761e
jeffbella <dev.jeffbella@gmail.com>
2020-07-02 07:05:27 -0500

    added function for cleaning non int, float data

--- list_comprehension.py
+++ list_comprehension.py
@@ -9,3 +9,14 @@ print(new_temps)
 new_temps = [temp/10 for temp in temps if temp > 240 and isinstance(temp, (int, float))]
 
 print(new_temps)
+
+#Define a function that takes a parameter a list that contains
+#both numbers and strings and returns the same list but with zeroes
+#instead of strings
+
+def foo(lst):
+    return [i if isinstance(i, (int, float)) else 0 for i in lst]
+
+#quick test
+temperatures = foo([99, 'no data', 95, 94, 'no data'])
+print(temperatures)
