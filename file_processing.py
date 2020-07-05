f5fa95bf7d033c611689324c38663cdbe4e50326
jeffbella <dev.jeffbella@gmail.com>
2020-07-05 12:15:01 -0500

    finished file_processing moving to working_with_imported_modules

--- file_processing.py
+++ file_processing.py
@@ -20,3 +20,22 @@ with open("%s/files/fruit_2.txt" % (here)) as myfile:
     content_2 = myfile.read()
 
 print(content_2)
+
+#writing data to file
+#could add user input to add vegatables to file later use input etc
+with open("%s/files/vegatables.txt" % (here), "w") as myfile:
+    myfile.write("Tomato\nCucumber\nOnion\n")
+
+#retrieving vegatables
+with open("%s/files/vegatables.txt" % (here)) as myfile:
+    content_3 = myfile.read()
+
+print(content_3)
+
+#appending
+#using the '+' to read the file and a .seek(0) method to move the 
+#cursor back to the first indicie of the file
+with open("%s/files/fruits.txt", "a+") as myfile:
+    myfile.write("\nOkra")
+    myfile.seek(0)
+    content = myfile.read()
