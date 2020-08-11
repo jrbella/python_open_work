00c062e8dcd71a173dd94a2cc308b48b1a9deea4
jeffbella <dev.jeffbella@gmail.com>
2020-08-10 22:16:30 -0500

    Adding enqueue and dequeue methods for Queue data structure

--- Nodes/queue.py
+++ Nodes/queue.py
@@ -1,20 +1,34 @@
 from node import Node
 
 class Queue:
-  # Add max_size and size properties within __init__():
   def __init__(self, max_size=None):
     self.head = None
     self.tail = None
     self.max_size = max_size
     self.size = 0
     
-  def peek(self):
-    if self.size > 0:
-      return self.head.get_value()
+  # Add your enqueue method below:
+  def enqueue(self, value):
+    if self.has_space():
+      item_to_add = Node(value)
+      print("Adding " + str(item_to_add.get_value()) + " to the queue!")
+      if self.is_empty():
+        self.head = item_to_add
+        self.tail = item_to_add
+      else:
+        self.tail.set_next_node(item_to_add)
+        self.tail = item_to_add
+      self.size += 1
     else:
-      print("Nothing to see here!")
+      print("Sorry, no more room!")
   
-  # Define get_size() and has_space() below:
+    
+  def peek(self):
+    if self.is_empty():
+      print("Nothing to see here!")
+    else:
+      return self.head.get_value()
+  
   def get_size(self):
     return self.size
   
@@ -26,3 +40,6 @@ class Queue:
     
   def is_empty(self):
     return self.size == 0
+
+q = Queue()
+q.enqueue("all the fluffy kitties")
