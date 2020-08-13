ef1d3b6360a2904eace06fbe348dc5d8c6e17854
jeffbella <dev.jeffbella@gmail.com>
2020-08-12 19:47:17 -0500

    finished hash mapping

--- hash_maps/hash_maps.py
+++ hash_maps/hash_maps.py
@@ -3,10 +3,10 @@ class HashMap:
     self.array_size = array_size
     self.array = [None for item in range(array_size)]
 
-  def hash(self, key):
+  def hash(self, key, count_collisions=0):
     key_bytes = key.encode()
     hash_code = sum(key_bytes)
-    return hash_code
+    return hash_code + count_collisions
 
   def compressor(self, hash_code):
     return hash_code % self.array_size
@@ -23,13 +23,63 @@ class HashMap:
       self.array[array_index] = [key, value]
       return
 
-    # current_array_value currently holds different key
+    # Collision!
+
+    number_collisions = 1
+
+    while(current_array_value[0] != key):
+      new_hash_code = self.hash(key, number_collisions)
+      new_array_index = self.compressor(new_hash_code)
+      current_array_value = self.array[new_array_index]
+
+      if current_array_value is None:
+        self.array[new_array_index] = [key, value]
+        return
+
+      if current_array_value[0] == key:
+        self.array[new_array_index] = [key, value]
+        return
+
+      number_collisions += 1
+
     return
 
   def retrieve(self, key):
     array_index = self.compressor(self.hash(key))
     possible_return_value = self.array[array_index]
-    if possible_return_value == None:
+
+    if possible_return_value is None:
       return None
-    elif possible_return_value[0] == key:
+
+    if possible_return_value[0] == key:
+      return possible_return_value[1]
+
+    retrieval_collisions = 1
+
+    while (possible_return_value != key):
+      new_hash_code = self.hash(key, retrieval_collisions)
+      retrieving_array_index = self.compressor(new_hash_code)
+      possible_return_value = self.array[retrieving_array_index]
+
+      if possible_return_value is None:
+        return None
+
+      if possible_return_value[0] == key:
         return possible_return_value[1]
+
+      number_collisions += 1
+
+    return
+
+
+"""
+Tests
+
+hash_map = HashMap(22)
+hash_map.assign('gabbro', 'igneous')
+hash_map.assign('sandstone', 'sedimentary')
+hash_map.assign('gneiss', 'metamorphic')
+
+print(hash_map.retrieve('gabbro'), hash_map.retrieve('sandstone'), hash_map.retrieve('gneiss'))
+
+"""
