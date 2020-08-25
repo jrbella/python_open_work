cfda2416943eb9a933903adf7e1b299d9a8cc96c
jeffbella <dev.jeffbella@gmail.com>
2020-08-25 05:00:43 -0500

    updating use case when bottles is singular bottle

--- ninety_nine_bottles_of_beer.py
+++ ninety_nine_bottles_of_beer.py
@@ -2,10 +2,18 @@
 
 def ninety_nine_bottles_of_beer():
     bottles = 99
-    while bottles > 1:
-        print(f"""  %s bottles of beer on the wall, 
-        %s bottles of beer, you take one down, 
-        you pass it around. %s bottles of beer on the wall""" % (bottles, bottles, (bottles - 1)))
+    word = "bottles"
+
+
+
+    while bottles > 0:
+        #Changing the plural
+        if bottles == 1:
+            word = "bottle"
+
+        print(f"""  %s %s of beer on the wall, 
+        %s %s of beer, you take one down, 
+        you pass it around. %s %s of beer on the wall""" % (bottles,word, bottles, word, (bottles - 1), word))
         bottles -= 1
         
 ninety_nine_bottles_of_beer()
